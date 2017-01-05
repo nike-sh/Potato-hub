cea8f4b0e27257ebaa8953cdde110c271edd2953
nike-sh <nikesh.tailor2@gmail.com>
2017-01-05 14:53:59 +0000

    Add files via upload

---
+++ Triangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfShapeCalculator
+{
+    class Triangle : ShapeProperties
+    {
+        public double sideA { get; set; }
+        public double sideB { get; set; }
+        public double angleC { get; set; }
+
+        public override double Area { get; set; }
+
+
+
+        public double GetArea()
+        {
+            Area = 0.5 * sideA * sideB * Math.Sin(angleC);
+            return Area;
+        }
+
+
+
+
+
+
+    }
+}
